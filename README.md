c972816ea95c68b89e94d5b2e4d7724ea91c62b4
Joshua Miles <joshua.m@hackerusa.com>
2020-08-27 15:54:50 -0500

    initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# Creating a Photo Diary
+
+This is an activity for FSWD Lesson 7.1.3- JSX
+
+Please refer to the Activity Guide in Canvas for directions.
---
+++ src/index.js
@@ -0,0 +1,48 @@
+let koalaContainer = document.createElement('div')
+
+koalaContainer.setAttribute('class', 'ui items')
+
+koalas.forEach(koala => {
+    let koalaCard = document.createElement('div')
+    koalaCard.setAttribute('class', 'item')
+    koalaCard.style.cursor = 'pointer';
+
+    // Koala Image
+    let imageContainer = document.createElement('div')
+    let koalaImage = document.createElement('img')
+
+    imageContainer.setAttribute('class', 'image')
+
+    koalaImage.setAttribute('src', koala.imageURL)
+
+    imageContainer.append(koalaImage)
+
+    // Koala Card Content
+    let koalaContent = document.createElement('div')
+    koalaContent.setAttribute('class', 'content')
+
+    let header = document.createElement('div')
+    header.setAttribute('class', 'header')
+    header.append(koala.name)
+
+    let descriptionParagraph = document.createElement('p')
+    descriptionParagraph.append(koala.description)
+
+    let descriptionContainer = document.createElement('div')
+    descriptionContainer.setAttribute('class', 'description')
+    descriptionContainer.append(descriptionParagraph)
+
+    koalaContent.append(
+        header,
+        descriptionContainer
+    )
+
+    koalaCard.append(
+        imageContainer,
+        koalaContent
+    )
+
+    koalaContainer.append(koalaCard)
+})
+
+document.body.append(koalaContainer)
