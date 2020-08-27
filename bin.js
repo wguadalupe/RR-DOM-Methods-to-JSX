716cb470ab7df3b6889faa388d6fcd490e4c766f
Joshua Miles <joshua.m@hackerusa.com>
2020-08-27 16:07:03 -0500

    added watch to babel

--- bin.js
+++ bin.js
@@ -2,7 +2,7 @@
 const open = require('open')
 const { exec, spawn } = require('child_process')
 exec('npx babel ./src --out-dir=./dist', () => {
-    spawn('npx', [ 'babel', './src', '--out-dir=./dist' ], { stdio: 'inherit' });
+    spawn('npx', [ 'babel', './src', '--out-dir=./dist', '--watch' ], { stdio: 'inherit' });
     open('./public/index.html')
     console.log('Watching for changes in "./src"')
 })
