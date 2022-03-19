a85d004053c856a5d888f60bcc23da542f52a3cf
Chance Embrey <cembreyfarquhar_sta@ku.edu>
2022-03-19 13:05:49 -0500

    use cross-spawn for windows compatibility

--- bin.js
+++ bin.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
+const spawn = require('cross-spawn')
 const open = require('open')
-const { exec, spawn } = require('child_process')
+const { exec } = require('child_process')
 exec('npx babel ./src --out-dir=./dist', () => {
     spawn('npx', [ 'babel', './src', '--out-dir=./dist', '--watch' ], { stdio: 'inherit' });
     open('./public/index.html')
