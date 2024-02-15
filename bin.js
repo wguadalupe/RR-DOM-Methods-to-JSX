0872bca17e1f72d6f6ec8c8cac06f1e71a1378d3
wguadalupe <w.guadalupe78@gmail.com>
2024-02-14 20:23:37 -0500

    dom methods

--- bin.js
+++ bin.js
@@ -1,9 +1,17 @@
 #!/usr/bin/env node
-const spawn = require('cross-spawn')
-const open = require('open')
-const { exec } = require('child_process')
-exec('npx babel ./src --out-dir=./dist', () => {
-    spawn('npx', [ 'babel', './src', '--out-dir=./dist', '--watch' ], { stdio: 'inherit' });
-    open('./public/index.html')
-    console.log('Watching for changes in "./src"')
-})
+const spawn = require('cross-spawn');
+const { exec } = require('child_process');
+
+// Wrap your logic inside an async function to use await
+async function main() {
+    const open = await import('open');
+
+    exec('npx babel ./src --out-dir=./dist', () => {
+        spawn('npx', ['babel', './src', '--out-dir=./dist', '--watch'], { stdio: 'inherit' });
+        // Use open.default here due to the way ESM modules are imported dynamically
+        open.default('./public/index.html').catch(console.error);
+        console.log('Watching for changes in "./src"');
+    });
+}
+
+main().catch(console.error);
--- src/index.js
+++ src/index.js
@@ -1,50 +1,24 @@
-let koalaContainer = document.createElement('div')
+import React from 'react';
+import koalas from './public/koalas';
 
-koalaContainer.setAttribute('class', 'ui items')
+const KoalaGallery = () => {
+  return (
+    <div>
+      {koalas.map(koala => (
+        <div key={koala.id} style={{ cursor: 'pointer' }}> {/* Assume each koala has a unique id */}
+          <div>
+            <img src={koala.imageURL} alt="Koala" />
+          </div>
+          <div>
+            <div className="header">{koala.name}</div>
+            <div className="description">
+              <p>{koala.description}</p>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-// Koala Card
-// koalas are in './public/koalas.js'
-koalas.forEach(koala => {
-    let koalaCard = document.createElement('div')
-    koalaCard.setAttribute('class', 'item')
-    koalaCard.style.cursor = 'pointer';
-
-    // Koala Card Image
-    let imageContainer = document.createElement('div')
-    let koalaImage = document.createElement('img')
-
-    imageContainer.setAttribute('class', 'image')
-
-    koalaImage.setAttribute('src', koala.imageURL)
-
-    imageContainer.append(koalaImage)
-
-    // Koala Card Content
-    let koalaContent = document.createElement('div')
-    koalaContent.setAttribute('class', 'content')
-
-    let header = document.createElement('div')
-    header.setAttribute('class', 'header')
-    header.append(koala.name)
-
-    let descriptionParagraph = document.createElement('p')
-    descriptionParagraph.append(koala.description)
-
-    let descriptionContainer = document.createElement('div')
-    descriptionContainer.setAttribute('class', 'description')
-    descriptionContainer.append(descriptionParagraph)
-
-    koalaContent.append(
-        header,
-        descriptionContainer
-    )
-
-    koalaCard.append(
-        imageContainer,
-        koalaContent
-    )
-
-    koalaContainer.append(koalaCard)
-})
-
-document.body.append(koalaContainer)
+export default KoalaGallery;
